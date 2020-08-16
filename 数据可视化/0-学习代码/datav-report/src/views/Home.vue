fd5e2b5fc36cf4ae80f2511a943c72bdfe05fb01
WangChiech <wangchiech@yeah.net>
2020-08-16 19:55:43 +0800

    数据可视化

---
+++ 数据可视化/0-学习代码/datav-report/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+  .home {
+    width: 100%;
+    height: 100%;
+    padding: 24px;
+    background-color: #eee;
+    box-sizing: border-box;
+  }
+</style>
